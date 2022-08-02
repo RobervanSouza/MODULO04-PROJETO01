17c6ecd9b2fae1e4fee73e76f8e134f6d6fa6ed3
RobervanSouza <you@example.com>
2022-08-02 15:26:24 -0300

    feito a contagem de numeros

--- convites-perola-negra/src/componets/Home.jsx
+++ convites-perola-negra/src/componets/Home.jsx
@@ -1,7 +1,29 @@
-import './Home.css'
+import "./Home.css";
+import sacola from "../assets/icons/sacola.svg";
+import logo from "../assets/logo.jpg";
+
+import ListaConvite from "./ListaConvite";
 
 function Home() {
-    return <div className="Home">Este é o componente Home</div>;
-  }
-  
-  export default Home; 
+  return (
+    <div className="Home">
+      <div className="header-header">
+        <div className="row">
+          <div className="header-logo">
+          <img src={logo} width= "70px" alt="logo" className="logo-icone"/>
+          <span className="logo-nome" >Perola Negra Convite </span>
+          </div>
+          <div className="sacola">
+          <img src={sacola} width= "70px" className="sacola-icone" alt=" sacola" />
+          </div>
+        </div>
+      </div>
+
+      <div className="Home__container">
+        <ListaConvite />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
